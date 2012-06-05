134c2c03cd25b64c1eb92e6384491a027c674c95
Mbugua <mbuguarufus@gmail.com>
2012-06-05 16:03:07 +0300

    New Views

---
+++ application/views/cakes.php
@@ -0,0 +1,86 @@
+<html>
+	<head>
+		<link href="<?php echo base_url(); ?>css/layout.css" rel="stylesheet" type="text/css" />
+		<!-- -->
+		<!-- Attach CSS files -->
+		<link rel="stylesheet" href="<?php echo base_url(); ?>css/layout.css"/>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>css/awesomebuttons.css"/>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>css/buttons.css"/>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>css/orbit.css"/>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>css/post.css"/>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>css/tabs.css"/>
+		<link rel="stylesheet" href="<?php echo base_url(); ?>css/form.css"/>
+		<!-- Attach JavaScript files -->
+		<script src="js/jquery-1.5.1.min.js" type="text/javascript"></script>
+		<script src="js/jquery.orbit.js" type="text/javascript"></script>
+		<title><?php echo $title ?></title>
+	</head>
+	<body>
+
+		<section class="current-body">
+			<section class="main">
+				<header>
+					<nav>
+						<ul>
+							<li>
+								<a>Home</a>
+							</li>
+							<li>
+								<a>Cakes</a>
+							</li>
+							<li>
+								<a>Articles</a>
+							</li>
+							<li>
+								<a>More Info.</a>
+							</li>
+						</ul>
+					</nav>
+				</header>
+				<section class="content">
+					<section class="search">
+						<input type="search" placeholder="Search"/>
+					</section>
+					<section class="tile-area">
+
+						<section class="main-page-tiles" style="margin-right:4.5%">
+
+						</section>
+						<section class="main-page-tiles">
+
+						</section>
+						<section class="right-align">
+							<section class="main-page-tiles" style="margin-right:0">
+
+							</section>
+
+						</section>
+
+					</section>
+					<section class="tile-area">
+
+						<section class="main-page-tiles" style="margin-right:4.5%">
+
+						</section>
+						<section class="main-page-tiles">
+
+						</section>
+						<section class="right-align">
+							<section class="main-page-tiles" style="margin-right:0">
+
+							</section>
+
+						</section>
+
+					</section>
+					<section class="paginate"></section>
+
+				</section>
+				<footer>
+
+				</footer>
+
+			</section>
+		</section>
+	</body>
+</html>
