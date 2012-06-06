ce3e0b51eb0f4720b442eeaa3e169d9e272dfd48
Mbugua <mbuguarufus@gmail.com>
2012-06-06 05:53:35 +0300

    Edits

--- application/controllers/c_front.php
+++ application/controllers/c_front.php
@@ -1,31 +1,35 @@
 <?php
 
 class C_Front extends CI_Controller {
-	var $data;
 
-	public function __construct() {
+	public function _construct() {
 
-		parent::__construct();
-	    $this->data=array();
+		parent::_construct();
+		$this -> load -> helper('url');
 
 	}
 
 	public function index() {
-		$data['form'] = '<p>You need to login.<p>';
+		$data['title'] = 'Welcome';
+		$data['content'] = "<p>Cakes Delights</p>";
 		$this -> load -> view('index', $data);
 	}//End of index file
-
-	public function vehicles() {
-		$data['status']="";
-		$data['response']="";
-		$data['form'] = '<p class="error"><br/><br/>No form has been chosen<br/><br/><p>';
-		$data['form_id']='';
-		$this -> load -> view('pages/vehicles/index', $data);
-		//echo 'Vehicles';
+	
+	public function cakes(){
+		$data['title'] = 'Cakes';
+		$data['content'] = "<p>Cakes Delights</p>";
+		$this -> load -> view('cakes', $data);
 	}
-
-	public function formviewer() {
-		$data['form'] = '<p class="error"><br/><br/>Choose a Form from the left panel to get started<br/><br/><p>';
-		$this -> load -> view('form', $data);
+	
+	public function articles(){
+		$data['title'] = 'Articles';
+		$data['content'] = "<p>Cakes Delights</p>";
+		$this -> load -> view('articles', $data);
 	}
-}?>
+public function moreinfo(){
+		$data['title'] = 'More Info.';
+		$data['content'] = "<p>Cakes Delights</p>";
+		$this -> load -> view('more_info', $data);
+	}
+}
+?>
--- application/views/index.php
+++ application/views/index.php
@@ -13,42 +13,60 @@
 		<!-- Attach JavaScript files -->
 		<script src="js/jquery-1.5.1.min.js" type="text/javascript"></script>
 		<script src="js/jquery.orbit.js" type="text/javascript"></script>
+		<title><?php echo $title; ?></title>
 	</head>
 	<body>
-		<section class="banner">
-			<section class="logo">
-				<img src="<?php echo base_url(); ?>images/coat_of_arms-resized.png" />
+
+		<section class="current-body">
+			<section class="main">
+				<header>
+					<nav>
+						<ul>
+							<li>
+								<a>Home</a>
+							</li>
+							<li>
+								<a>Cakes</a>
+							</li>
+							<li>
+								<a>Articles</a>
+							</li>
+							<li>
+								<a>More Info.</a>
+							</li>
+						</ul>
+					</nav>
+				</header>
+				<section class="content">
+					<section class="image-scroller">
+
+					</section>
+					<section class="tile-area">
+						
+						<section class="main-page-tiles" style="margin-right:4.5%">
+
+						</section>
+						
+						<section class="main-page-tiles">
+
+						</section>
+						
+						<section class="right-align">
+							<section class="main-page-tiles" style="margin-right:0">
+
+						</section>
+							
+						</section>
+						
+
+					</section>
+
+				</section>
+				<footer>
+
+				</footer>
 
 			</section>
-			<section class="credentials">
-				<section class="title">
-					Ministry Of Health 
-				</section>
-				<section class="subtitle">
-					Department of Family Health<p></p>
-				</section>
-				<section class="division">
-					Division of Nutrition<p></p><p></p>
-				</section>
-			</section>
-			<section class="date">
-				
-				 <?php echo date("l F d, Y"); ?>
-			</section>
-		</section>
-
-		<section class="message">
-			<?php echo $form; ?>
-		</section>
-		<section class="login">
-			<form class="form-login" method="post">
-				<input name="username" type="text" placeholder="Username / Email Address"/>
-				<p></p>
-				<input  name="secret" type="password" placeholder="Password"/>
-				<p></p>
-				<button type="submit" class="awesome myblue large" formaction="<?php echo base_url().'c_auth/login'?>" />
-				Login</button>
-			</form>
 		</section>
 	</body>
 </html>
