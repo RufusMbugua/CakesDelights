36eb833e0f80e54dabcbb6084c0cc0a822a1ebc1
Mbugua <mbuguarufus@gmail.com>
2012-06-11 04:41:33 +0300

    Editing Nav

---
+++ application/views/navigation.php
@@ -0,0 +1,20 @@
+<?php ?>
+<nav>
+	<ul>
+		<li>
+			<?php echo anchor(base_url() . 'c_front/index', 'Home'); ?>
+		</li>
+		<li>
+			<?php echo anchor(base_url() . 'c_front/cakes', 'Cakes'); ?>
+		</li>
+		<li>
+			<?php echo anchor(base_url() . 'c_front/articles', 'Articles'); ?>
+		</li>
+		<li>
+			<a>Orders</a>
+		</li>
+		<li>
+			<?php echo anchor(base_url() . 'c_front/moreinfo', 'More Info.'); ?>
+		</li>
+	</ul>
+</nav>
